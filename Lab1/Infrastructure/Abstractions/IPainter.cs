471c36e22371ce327f3b039d9544d80bbdcbc5e6
Rukin Danil <rukin2018d@yandex.ru>
2021-09-15 19:39:38 +0700

    Класс рисовальщик

---
+++ Lab1/Infrastructure/Abstractions/IPainter.cs
@@ -0,0 +1,16 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Lab1.Infrastructure.Abstractions
+{
+	public interface IPainter<T> where T: struct, IEquatable<T>, IFormattable
+	{
+		void Draw(Figure<T> figure, Matrix<T> finalTranformationMatrix, Graphics graphics);
+		void Draw(Figure<T> figure, List<Matrix<T>> tranfromationMatrices, Graphics graphics);
+		void Draw(Figure<T> figure, ITransformation<T> finalTransformation, Parameters<T> parameter, Graphics graphics);
+		void Draw(Figure<T> figure, List<ITransformation<T>> transformations, List<Parameters<T>> parameters, Graphics graphics);
+	}
+}
---
+++ Lab1/Infrastructure/Painters/DPainter.cs
@@ -0,0 +1,43 @@
+﻿using Lab1.Infrastructure.Abstractions;
+using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Lab1.Infrastructure.Painters
+{
+	public class DPainter : IPainter<double>
+	{
+		public void Draw(Figure<double> figure, Matrix<double> finalTranformationMatrix, Graphics graphics)
+		{
+			figure.Vertexes = finalTranformationMatrix * figure.Vertexes;
+			figure.Draw(graphics);
+		}
+
+		public void Draw(Figure<double> figure, List<Matrix<double>> tranfromationMatrices, Graphics graphics)
+		{
+			Matrix<double> finalMatrix = CreateMatrix.Dense(4, 4, 1.0);
+			foreach (var matrix in tranfromationMatrices)
+			{
+				finalMatrix *= matrix;
+			}
+			Draw(figure, finalMatrix, graphics);
+		}
+
+		public void Draw(Figure<double> figure, ITransformation<double> finalTransformation, Parameters<double> parameter, Graphics graphics)
+		{
+			Draw(figure, finalTransformation.GetTransformation(parameter), graphics);
+		}
+
+		public void Draw(Figure<double> figure, List<ITransformation<double>> transformations, List<Parameters<double>> parameters, Graphics graphics)
+		{
+			Matrix<double> finalMatrix = CreateMatrix.Dense(4, 4, 1.0);
+			for (int i = 0; i < transformations.Count; i++)
+			{
+				finalMatrix *= transformations[i].GetTransformation(parameters[i]);
+			}
+			Draw(figure, finalMatrix, graphics);
+		}
+	}
+}
